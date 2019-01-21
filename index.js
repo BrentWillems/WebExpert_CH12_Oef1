ec7a5d6b81cde229eca25ce5b2a1cb5e591ef117
brent <brent.willems.bw@gmail.com>
2019-01-21 10:31:32 +0100

    added optional seeding

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+var express = require('express');
+var bodyParser = require('body-parser');
 var db = require('./db');
-var Car = require('./models/cars')
+var Car = require('./models/cars');
 
+var app = express();
+app.use(bodyParser.json());
 
-// var Car = mongoose.model('Car', {manufacturer: String, model: String, price: Number, wiki: String}); //model
-var car1 = new Car({manufacturer: 'Porsche', model: '911', price: 135000, wiki: 'http://en.wikipedia.org/wiki/Porsche_997'});
-var car2 = new Car({manufacturer: 'Tesla', model: 'model 3', price: 30000, wiki: 'https://nl.wikipedia.org/wiki/Tesla_Model_3'});
-var car3 = new Car({manufacturer: 'Tesla', model: 'model S', price: 100000, wiki: 'https://nl.wikipedia.org/wiki/Tesla_Model_S'});
-
-Car.create(car1, car2, car3);
+var seed = false;
+if (seed) {
+    var car1 = new Car({manufacturer: 'Porsche', model: '911', price: 135000, wiki: 'http://en.wikipedia.org/wiki/Porsche_997'});
+    var car2 = new Car({manufacturer: 'Tesla', model: 'model 3', price: 30000, wiki: 'https://nl.wikipedia.org/wiki/Tesla_Model_3'});
+    var car3 = new Car({manufacturer: 'Tesla', model: 'model S', price: 100000, wiki: 'https://nl.wikipedia.org/wiki/Tesla_Model_S'});
+    Car.create(car1, car2, car3);
+}
