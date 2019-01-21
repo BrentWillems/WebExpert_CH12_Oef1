87ad357a64f0e9537b42ebfa9a8370c1f241508c
brent <brent.willems.bw@gmail.com>
2019-01-21 10:25:21 +0100

    extracted models

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-var mongoose = require('mongoose');
+var db = require('./db');
+var Car = require('./models/cars')
 
-var Car = mongoose.model('Car', {manufacturer: String, model: String, price: Number, wiki: String}); //model
+
+// var Car = mongoose.model('Car', {manufacturer: String, model: String, price: Number, wiki: String}); //model
 var car1 = new Car({manufacturer: 'Porsche', model: '911', price: 135000, wiki: 'http://en.wikipedia.org/wiki/Porsche_997'});
 var car2 = new Car({manufacturer: 'Tesla', model: 'model 3', price: 30000, wiki: 'https://nl.wikipedia.org/wiki/Tesla_Model_3'});
 var car3 = new Car({manufacturer: 'Tesla', model: 'model S', price: 100000, wiki: 'https://nl.wikipedia.org/wiki/Tesla_Model_S'});
